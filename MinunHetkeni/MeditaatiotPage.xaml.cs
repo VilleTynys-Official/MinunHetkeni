835ef831ae06e2ab5a683890e4d04a798e568576
V.Tynys <ville.tynys@yahoo.com>
2020-04-19 13:59:00 +0300

    Lisätty MeditaatiotPage.

---
+++ MinunHetkeni/MeditaatiotPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MinunHetkeni
+{
+    public partial class MeditaatiotPage : ContentPage
+    {
+        public MeditaatiotPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

a471a61feb8d6accdb899e4dd031c00ad8d248c8
V.Tynys <ville.tynys@yahoo.com>
2020-04-22 02:48:20 +0300

    Luotu sivu karuselli onboardingia varten.

---
+++ MinunHetkeni/Views/KaruselliPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MinunHetkeni.Views
+{
+    public partial class KaruselliPage : ContentPage
+    {
+        public KaruselliPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

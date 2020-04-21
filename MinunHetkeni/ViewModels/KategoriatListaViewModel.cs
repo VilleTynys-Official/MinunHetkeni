c70ad48f3239094de3a48c037a81b635219200a9
V.Tynys <ville.tynys@yahoo.com>
2020-04-21 22:15:41 +0300

    Kategoria luokka otettu käyttöön.

--- MinunHetkeni/ViewModels/KategoriatListaViewModel.cs
+++ MinunHetkeni/ViewModels/KategoriatListaViewModel.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+
 namespace MinunHetkeni.ViewModels
 {
     public class KategoriatListaViewModel
     {
+
+        public ObservableCollection<Kategoria> Kategoriat { get; set; }
+        
+
         public KategoriatListaViewModel()
         {
+            Kategoriat = new ObservableCollection<Kategoria>();
 
-
-
+            Kategoriat.Add(new Kategoria("Aloitus", "ylaosa.png",
+                "Aloita tästä seitsemän päivän tutustumisjakso. Yhden harjoituksen kesto noin 10 minuuttia."));
         }
     }
 }

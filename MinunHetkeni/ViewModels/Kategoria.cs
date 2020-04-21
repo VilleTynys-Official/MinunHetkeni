d52d37d4ad8dec67b822d9d6536b4091b4e7aa84
V.Tynys <ville.tynys@yahoo.com>
2020-04-21 22:14:56 +0300

    Luotu Kategoria-luokka jonka avulla voidaan näyttää eri kategorioita.

---
+++ MinunHetkeni/ViewModels/Kategoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MinunHetkeni.ViewModels
+{
+    public class Kategoria
+    {
+        public string KategorianNimi { get; set; } //määritellään propertyt (eli fieldsit, joilla on get set accessorit)
+        public string KategorianKuva { get; set; }
+        public string KategorianKuvaus { get; set; }
+
+        public Kategoria(string kategorianNimi, string kategorianKuva, string kategorianKuvaus)
+        {
+
+            KategorianNimi = kategorianNimi;        //käytetään accessoreja
+            KategorianKuva = kategorianKuva;
+            KategorianKuvaus = kategorianKuvaus;
+        }
+    }
+}

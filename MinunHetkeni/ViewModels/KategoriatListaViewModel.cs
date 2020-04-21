a3da4c038572e1872aa22e485fee7d84630df3a2
V.Tynys <ville.tynys@yahoo.com>
2020-04-22 00:02:37 +0300

    Jotain git bugeja.

--- MinunHetkeni/ViewModels/KategoriatListaViewModel.cs
+++ MinunHetkeni/ViewModels/KategoriatListaViewModel.cs
@@ -15,6 +15,12 @@ namespace MinunHetkeni.ViewModels
 
             Kategoriat.Add(new Kategoria("Ensimmäiset askeleet", "kategoria1.png",
                 "Aloita tästä seitsemän päivän tutustumisjakso. Yhden harjoituksen kesto noin 10 minuuttia."));
+
+            Kategoriat.Add(new Kategoria("Seuraavat askeleet", "kategoria2.png",
+                "Jatka tästä tutustumisjakson jälkeen. Yhden harjoituksen kesto noin 10 minuuttia."));
+
+            Kategoriat.Add(new Kategoria("Tutustuminen Metta-meditointiin", "kategoria3.png",
+                "Tässä kolmannessa polussa tutustut Metta-meditointiin. Yhden harjoituksen kesto noin 10 minuuttia"));
         }
     }
 }

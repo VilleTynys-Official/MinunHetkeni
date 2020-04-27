a508bb31c3b436d233efc352314911c6dae73de4
V.Tynys <ville.tynys@yahoo.com>
2020-04-27 19:51:51 +0300

    Väliaikainen iconi lisätty.

--- MinunHetkeni.Android/MainActivity.cs
+++ MinunHetkeni.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using MediaManager;
 
 namespace MinunHetkeni.Droid
 {
-    [Activity(Label = "MinunHetkeni", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MinunHetkeni", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
